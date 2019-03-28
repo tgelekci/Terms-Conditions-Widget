7c26f689f8d8466febcd174de280a68b829ff370
Taylan <t.gelekci08@gmail.com>
2019-03-28 17:03:25 +0300

    Terms&Conditions Widget
    A Simple Terms&Conditions Widget

---
+++ main.js
@@ -0,0 +1,12 @@
+function checkedBox(){
+    if(document.getElementById("chcbx").checked)
+    {   
+        window.location.href="https://www.jotform.com/";
+    }
+    
+}
+
+function textareaText(text){
+    document.getElementById("txtr").value=text;
+    var newtext=text;
+}
